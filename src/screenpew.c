8314c3b3c1eaa5b619e13186cf08c404a221fa10
cod1r <jasonho353@gmail.com>
2022-03-22 18:54:48 -0500

    translated proper mouse coordinates for the case of multiple monitors, deleted some functions that we probably aren't going to use again

--- src/screenpew.c
+++ src/screenpew.c
@@ -27,19 +27,12 @@ void create_png(uint8_t *img_data, xcb_window_t window, int width, int height) {
   strcpy(home_plus_file_name, home);
   strcat(home_plus_file_name, file_name);
   png_image_write_to_file(
-    &img, 
-    home_plus_file_name, 
-    0, 
-    img_data, 
-    4 * width, 
-    NULL
+    &img,    home_plus_file_name,    0,    img_data,    4 * width,    NULL
   );
 }
 
 void get_image(
-  xcb_connection_t *connection, 
-  xcb_window_t window, 
-  int x1,
+  xcb_connection_t *connection,  xcb_window_t window,  int x1,
   int y1,
   int x2,
   int y2
@@ -49,9 +42,7 @@ void get_image(
   int min_x = min(x1, x2);
   int max_x = max(x1, x2);
   xcb_get_image_cookie_t cookie = xcb_get_image(
-    connection, 
-    XCB_IMAGE_FORMAT_Z_PIXMAP, 
-    window,
+    connection,    XCB_IMAGE_FORMAT_Z_PIXMAP,    window,
     min_x,
     min_y,
     max_x - min_x,
@@ -71,72 +62,10 @@ void get_image(
   }
 }
 
-void print_window_name(xcb_connection_t *connection, xcb_window_t window) {
-  char *atom_name = "_NET_WM_NAME";
-  xcb_intern_atom_cookie_t atom_cookie = xcb_intern_atom(connection, 1, strlen(atom_name), atom_name);
-  xcb_intern_atom_reply_t *atom_reply = xcb_intern_atom_reply(connection, atom_cookie, NULL);
-
-  xcb_get_property_cookie_t prop_cookie = xcb_get_property(connection, 0, window, atom_reply->atom, XCB_ATOM_ANY, 0, 40);
-  xcb_get_property_reply_t *prop_reply = xcb_get_property_reply(connection, prop_cookie, NULL);
-  if (prop_reply != NULL) {
-    int name_length = xcb_get_property_value_length(prop_reply);
-    char *value = xcb_get_property_value(prop_reply);
-    for (int i = 0; i < name_length; i++) {
-      printf("%c", value[i]);
-    }
-    printf("\n");
-  }
-}
-
-void print_window_state(xcb_connection_t *connection, xcb_window_t window) {
-  printf("printing window state...\n");
-  char *atom_state = "_NET_WM_STATE";
-  xcb_intern_atom_cookie_t atom_cookie = xcb_intern_atom(connection, 1, strlen(atom_state), atom_state);
-  xcb_intern_atom_reply_t *atom_reply = xcb_intern_atom_reply(connection, atom_cookie, NULL);
-
-  xcb_get_property_cookie_t prop_cookie = xcb_get_property(connection, 0, window, atom_reply->atom, XCB_ATOM_ANY, 0, 40);
-  xcb_get_property_reply_t *prop_reply = xcb_get_property_reply(connection, prop_cookie, NULL);
-  if (prop_reply->type != 0) {
-    int value_len = xcb_get_property_value_length(prop_reply);
-    int *value = xcb_get_property_value(prop_reply);
-    printf("format: %d\n", prop_reply->format);
-    printf("value len: %d, %d\n", value_len, prop_reply->length);
-    printf("bytes after: %d\n", prop_reply->bytes_after);
-    printf("value of state: %d\n", *value);
-    xcb_get_atom_name_cookie_t name_cookie = xcb_get_atom_name(connection, *value);
-    xcb_get_atom_name_reply_t *name_reply = xcb_get_atom_name_reply(connection, name_cookie, NULL);
-    if (name_reply != NULL) {
-      char *atom_name_str = xcb_get_atom_name_name(name_reply);
-      int atom_name_len = xcb_get_atom_name_name_length(name_reply);
-      for (int i = 0; i < atom_name_len; i++) {
-        printf("%c", atom_name_str[i]);
-      }
-      printf("\n");
-    }
-  }
-}
-
-void traverse_windows(xcb_connection_t *connection, xcb_window_t window) {
-  xcb_query_tree_cookie_t query_cookie = xcb_query_tree(connection, window);
-  xcb_query_tree_reply_t *query_reply = xcb_query_tree_reply(connection, query_cookie, NULL);
-  xcb_window_t *children = xcb_query_tree_children(query_reply);
-  printf("window: %d\n", window);
-
-  print_window_state(connection, window);
-  print_window_name(connection, window);
-
-  for (int i = 0; i < query_reply->children_len; i++) {
-    traverse_windows(connection, children[i]);
-  }
-}
-
 void add_mouse_event(xcb_connection_t *connection, xcb_window_t window) {
   uint32_t values[] = {XCB_EVENT_MASK_BUTTON_PRESS | XCB_EVENT_MASK_BUTTON_RELEASE};
   xcb_change_window_attributes(
-    connection, 
-    window, 
-    XCB_CW_EVENT_MASK, 
-    values
+    connection,    window,    XCB_CW_EVENT_MASK,    values
   );
 }
 
@@ -146,59 +75,60 @@ int main() {
 
   xcb_screen_iterator_t iter = xcb_setup_roots_iterator(setup);
   xcb_window_t root = iter.data->root;
-  while (iter.rem != 0) {
-    xcb_get_geometry_cookie_t geo_cookie = xcb_get_geometry(connection, iter.data->root);
-    xcb_get_geometry_reply_t *geo_reply = xcb_get_geometry_reply(connection, geo_cookie, NULL);
+	xcb_window_t window = xcb_generate_id(connection);
+	xcb_create_window(
+		connection,
+		0,
+		window,
+		iter.data->root,
+		0,
+		0,
+		iter.data->width_in_pixels,
+		iter.data->height_in_pixels,
+		0,
+		XCB_WINDOW_CLASS_INPUT_OUTPUT,		iter.data->root_visual,
+		0,
+		NULL
+	);
+	xcb_intern_atom_cookie_t intern_state_cookie = xcb_intern_atom(
+			connection, 1, strlen("_NET_WM_STATE"), "_NET_WM_STATE"
+	);
+	xcb_intern_atom_reply_t *intern_state_reply = xcb_intern_atom_reply(
+			connection, intern_state_cookie, NULL
+	);
 
-    xcb_window_t window = xcb_generate_id(connection);
-    xcb_create_window(
-      connection,
-      0,
-      window,
-      iter.data->root,
-      0,
-      0,
-      geo_reply->width,
-      geo_reply->height,
-      0,
-      XCB_WINDOW_CLASS_INPUT_OUTPUT, 
-      iter.data->root_visual,
-      0,
-      NULL
-    );
-    xcb_intern_atom_cookie_t intern_state_cookie = xcb_intern_atom(connection, 1, strlen("_NET_WM_STATE"), "_NET_WM_STATE");
-    xcb_intern_atom_reply_t *intern_state_reply = xcb_intern_atom_reply(connection, intern_state_cookie, NULL);
+	xcb_intern_atom_cookie_t intern_fullscrn_cookie = xcb_intern_atom(
+			connection, 1, strlen("_NET_WM_STATE_FULLSCREEN"), "_NET_WM_STATE_FULLSCREEN"
+	);
+	xcb_intern_atom_reply_t *intern_fullscrn_reply = xcb_intern_atom_reply(
+			connection, intern_fullscrn_cookie, NULL
+	);
 
-    xcb_intern_atom_cookie_t intern_fullscrn_cookie = xcb_intern_atom(connection, 1, strlen("_NET_WM_STATE_FULLSCREEN"), "_NET_WM_STATE_FULLSCREEN");
-    xcb_intern_atom_reply_t *intern_fullscrn_reply = xcb_intern_atom_reply(connection, intern_fullscrn_cookie, NULL);
-
-    int atom_states[] = {intern_fullscrn_reply->atom};
-    // cannot put XCB_ATOM_ANY into change property. WHY!!!!!!
-    // there has to be someway to get ATOM TYPE. If not, this is going to be super confusing moving on...
-    xcb_change_property(
-      connection,
-      XCB_PROP_MODE_APPEND,
-      window,
-      intern_state_reply->atom,
-      XCB_ATOM_ATOM,
-      32,
-      1,
-      atom_states
-    );
-    xcb_change_property (
-      connection,
-      XCB_PROP_MODE_REPLACE,
-      window,
-      XCB_ATOM_WM_NAME,
-      XCB_ATOM_STRING,
-      8,
-      strlen ("screenpew"),
-      "screenpew" 
-    );
-    add_mouse_event(connection, window);
-    xcb_map_window(connection, window);
-    xcb_screen_next(&iter);
-  }
+	int atom_states[] = {intern_fullscrn_reply->atom};
+	// cannot put XCB_ATOM_ANY into change property. WHY!!!!!!
+	// there has to be someway to get ATOM TYPE. If not, this is going to be super confusing moving on...
+	xcb_change_property(
+		connection,
+		XCB_PROP_MODE_APPEND,
+		window,
+		intern_state_reply->atom,
+		XCB_ATOM_ATOM,
+		32,
+		1,
+		atom_states
+	);
+	xcb_change_property (
+		connection,
+		XCB_PROP_MODE_REPLACE,
+		window,
+		XCB_ATOM_WM_NAME,
+		XCB_ATOM_STRING,
+		8,
+		strlen ("screenpew"),
+		"screenpew"	);
+	add_mouse_event(connection, window);
+	xcb_map_window(connection, window);
+	xcb_screen_next(&iter);
   xcb_flush(connection);
   xcb_generic_event_t *event;
   int press_x;
@@ -217,7 +147,17 @@ int main() {
     }
     free(event);
   }
-  get_image(connection, root, press_x, press_y, release_x, release_y);
+	xcb_translate_coordinates_reply_t *trans1 = xcb_translate_coordinates_reply (
+			connection,	
+			xcb_translate_coordinates(connection, window, root, press_x, press_y),
+			NULL
+	);
+	xcb_translate_coordinates_reply_t *trans2 = xcb_translate_coordinates_reply (
+			connection,
+			xcb_translate_coordinates(connection, window, root, release_x, release_y),
+			NULL
+	);
+  get_image(connection, root, trans1->dst_x, trans1->dst_y, trans2->dst_x, trans2->dst_y);
   xcb_disconnect(connection);
   return 0;
 }
